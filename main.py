8231dc976dee1956969c4a5582f1cee09cfc6c5d
oklixm <liamsmydo@gmail.com>
2023-11-20 19:35:28 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,45 @@
 import socket
 import threading
-import multiprocessing
+import re
 
 
 print_lock = threading.Lock()
 
+def valid_ip(arg):
+    ip_pattern = re.compile(
+        r'^(25[0-5]|2[0-4][0-9]|[0-1]?[0-9][0-9]?)\.'
+        r'(25[0-5]|2[0-4][0-9]|[0-1]?[0-9][0-9]?)\.'
+        r'(25[0-5]|2[0-4][0-9]|[0-1]?[0-9][0-9]?)\.'
+        r'(25[0-5]|2[0-4][0-9]|[0-1]?[0-9][0-9]?)$'
+    )
+    hostname_pattern = re.compile(
+        r'^[a-zA-Z0-9]([a-zA-Z0-9\-]{0,61}[a-zA-Z0-9])?(\.[a-zA-Z]{2,})+$'
+    )
+
+    if ip_pattern.match(arg) or hostname_pattern.match(arg):
+        return True
+    else:
+        return False
+
+def valid_port(arg):
+    try:
+        ports = parse_ports(arg)
+        for port in ports:
+            if not (0 < int(port) <= 65535):
+                return False
+        return True
+    except ValueError:
+        return False
+
+def parse_ports(arg):
+    ports = []
+    for part in arg.split(","):
+        if '-' in part:
+            start, end = map(int, part.split('-'))
+            ports.extend(range(start, end + 1))
+        else:
+            ports.append(int(part))
+    return ports
 
 def scan_port(target, port):
     try:
@@ -30,10 +65,28 @@ def scan_port(target, port):
 def thread_scan(target, ports):
     for port in ports:
         scan_port(target, port)
+        
+def ip_input():
+    while True:
+        target = input("Enter the target IP or hostname: ")
+        if valid_ip(target):
+            print ("you have entered a valid ip address")
+            return target
+        else:
+            print ("please enter a valid ip address")
+
+def port_input():
+    while True:
+        port_range = input("Enter the port range (ex. '80-100' or '23,80,445') ")
+        if valid_port(port_range):
+            print ("you have entered a valid port range")
+            return port_range
+        else:
+            print ("please enter a valid port range")
 
 def main():
-    target = input("Enter the target IP or hostname: ")
-    port_range = input("Enter the port range (ex. '80-100' or '23,80,445') ")
+    target = ip_input()
+    port_range = port_input()
 
     ports = []                          #Specifies the port list to scan
     for part in port_range.split(","):
